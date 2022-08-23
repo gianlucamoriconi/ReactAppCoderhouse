9acff906b220d34ca6d7339a57846af69a7429c2
Gianluca Moriconi <gianlucamoriconi@MBP-de-usuario.fibertel.com.ar>
2022-08-22 22:21:15 -0300

    Probando fix de error en deploy

--- src/navbar.js
+++ src/navbar.js
@@ -2,39 +2,39 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Alf's Store</a>
+                <a className="navbar-brand" href>Alf's Store</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">Inicio</a>
+                    <a className="nav-link active" aria-current="page" href>Inicio</a>
                     </li>
                     <li className="nav-item">
-                    <a className="nav-link" href="#">Todos los productos</a>
+                    <a className="nav-link" href>Todos los productos</a>
                     </li>
                     <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <a className="nav-link dropdown-toggle" href id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             Accesorios
                         </a>
                         <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a className="dropdown-item" href="#">Tazas</a></li>
-                            <li><a className="dropdown-item" href="#">Mates</a></li>
+                            <li><a className="dropdown-item" href>Tazas</a></li>
+                            <li><a className="dropdown-item" href>Mates</a></li>
                             <li><hr className="dropdown-divider" /></li>
-                            <li><a className="dropdown-item" href="#">Quiero algo que no encontré</a></li>
+                            <li><a className="dropdown-item" href>Quiero algo que no encontré</a></li>
                         </ul>
                     </li>
                     <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <a className="nav-link dropdown-toggle" href id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             Indumentaria
                         </a>
                         <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a className="dropdown-item" href="#">Remeras</a></li>
-                            <li><a className="dropdown-item" href="#">Buzos</a></li>
-                            <li><a className="dropdown-item" href="#">Pantalones</a></li>
+                            <li><a className="dropdown-item" href>Remeras</a></li>
+                            <li><a className="dropdown-item" href>Buzos</a></li>
+                            <li><a className="dropdown-item" href>Pantalones</a></li>
                             <li><hr className="dropdown-divider" /></li>
-                            <li><a className="dropdown-item" href="#">Quiero algo que no encontré</a></li>
+                            <li><a className="dropdown-item" href>Quiero algo que no encontré</a></li>
                         </ul>
                     </li>
                 </ul>
