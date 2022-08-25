8e26e753a0e32ee69aeabf8a9294992d6e9b90ef
Gianluca Moriconi <gianlucamoriconi@MBP-de-usuario.fibertel.com.ar>
2022-08-24 22:02:06 -0300

    Component grid

--- src/components/bodyhome.js
+++ src/components/bodyhome.js
@@ -1,3 +1,5 @@
+import Griditems from './grid-items';
+
 const Bodyhome = () => {
      
     return (
@@ -5,6 +7,7 @@ const Bodyhome = () => {
         <div className="container p-4">
             <h1 className="pt-4 text-light">La tienda de Alf!</h1>
         </div>
+        <Griditems />
         </>
     )
 }
---
+++ src/components/grid-items.js
@@ -0,0 +1,12 @@
+
+const Griditems = () => {
+    return (
+        <>
+        <div className="container example-container-grid">
+            {/*Aca llamaremos a cada item como componente desde item-product.js */}
+        </div>
+        </>        
+    )
+}
+
+export default Griditems;
