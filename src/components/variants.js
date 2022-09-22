ba6633faa32210cd6e15fc4d3a3f3da268c8b0de
Gianluca Moriconi <gianlucamoriconi@MacBook-Pro-de-usuario.local>
2022-09-22 00:02:09 -0300

    Style

--- src/components/variants.js
+++ src/components/variants.js
@@ -24,7 +24,7 @@ const Variants = ({property1, value1, property2, value2, setOption1, setOption2}
         return (
             <>
             <div className="variants mb-5">
-                <div id="values-1" className="options-propery-1 mb-3">
+                <div id="values-1" className="options-propery-1 mb-5">
                     <h3 className="property-title mb-4 fs-6">{property1}</h3>
                     <div className="d-flex flex-wrap">
                         <Select property="propiedad-1" options={value1} onSelect={setOption1}/>
