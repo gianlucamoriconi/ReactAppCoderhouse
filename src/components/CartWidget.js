f7686c67de1b0b6f150f1104107b3bc4adbec120
Gianluca Moriconi <gianlucamoriconi@MacBook-Pro-de-usuario.local>
2022-10-25 10:01:39 -0300

    Migrate Cart widget to react bootstrap offcanvas

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -1,25 +1,31 @@
 import { BsCart2 } from 'react-icons/bs';
 import { IoMdTrash } from 'react-icons/io';
-import { useContext } from "react";
+import { useState, useContext } from "react";
 import { CartContext } from "../context/cartContext";
 import { Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 
 const CartWidget = () => {
     const { cart, removeItem, removeAllItems, totalAmountInCart } = useContext(CartContext);
 
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
         return (
             <div id="cartWidget">
-                <a className="btn btn-primary" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">
-                <div className='d-flex'><BsCart2 /><span className='cart-widget-amount'>{cart.length}</span></div>
-                </a>
+                <Button className="btn btn-primary" onClick={handleShow}>
+                    <div className='d-flex'><BsCart2 /><span className='cart-widget-amount'>{cart.length}</span></div>
+                </Button>
 
-                <div className="offcanvas offcanvas-end" tabIndex={-1} id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
-                    <div className="offcanvas-header">
-                        <h5 className="offcanvas-title fw-bold" id="offcanvasExampleLabel">Carrito</h5>
-                        <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                    </div>
-                    <div className="offcanvas-body">
-                        {cart.length < 1 ? <div>
+                <Offcanvas show={show} placement="end" onHide={handleClose}>
+                    <Offcanvas.Header closeButton>
+                    <Offcanvas.Title className="fw-bold">Carrito</Offcanvas.Title>
+                    </Offcanvas.Header>
+                    <Offcanvas.Body>
+                    {cart.length < 1 ? <div>
                             Aquí estarán los productos que llevas comprando
                         </div>:
                         cart.map((item, i) => (
@@ -49,23 +55,23 @@ const CartWidget = () => {
                             </div>
                         ))
                         }
-                    </div>
+                    </Offcanvas.Body>
                     {cart.length > 0 ?
-                    <div className='w-100 d-flex p-3 flex-wrap'>
-                        <div className='p-3 d-flex w-100'>
-                            <h4 className='text-end w-100'>Total: ${new Intl.NumberFormat('es-AR').format(totalAmountInCart())}</h4>
-                            <hr/>
+                        <div className='w-100 d-flex p-3 flex-wrap'>
+                            <div className='p-3 d-flex w-100'>
+                                <h4 className='text-end w-100'>Total: ${new Intl.NumberFormat('es-AR').format(totalAmountInCart())}</h4>
+                                <hr/>
+                            </div>
+                            <div className='p-3 d-flex w-100'>
+                                <button onClick={removeAllItems} className="btn btn-secondary w-100 me-2">Vaciar carrito</button>
+                                <Link to="/cart" className="btn btn-primary w-100" onClick={handleClose}>Ver carrito</Link>
+                            </div>
+                            <div className='ps-3 pe-3 pt-0 d-flex w-100'>
+                            <Link to="/checkout/datos" onClick={handleClose} className="btn btn-primary w-100">Iniciar compra</Link>
+                            </div>
                         </div>
-                        <div className='p-3 d-flex w-100'>
-                            <button onClick={removeAllItems} className="btn btn-secondary w-100 me-2">Vaciar carrito</button>
-                            <Link to="/cart" className="btn btn-primary w-100">Ver carrito</Link>
-                        </div>
-                        <div className='ps-3 pe-3 pt-0 d-flex w-100'>
-                        <Link to="/checkout/datos" className="btn btn-primary w-100">Iniciar compra</Link>
-                        </div>
-                    </div>
                     : null}
-                </div>
+                </Offcanvas>
             </div>
         )
 
