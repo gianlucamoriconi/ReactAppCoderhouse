3f9eba27a56e6579645e2ce9c75c5eff6f6f0ab3
Gianluca Moriconi <gianlucamoriconi@MacBook-Pro-de-usuario.local>
2022-10-25 11:00:19 -0300

    Message when there isnt products in category

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,13 +1,22 @@
 import ItemProduct from "./ItemProduct"
 
 const ItemList = ( {products = []} ) => {
-    return (
-        <div className="row p-3 justify-content-center">
-            {products.map((prod) => {
-                return <ItemProduct product={prod} key={prod.id}/>
-            })}
-        </div>
-    )
+
+    if (products.length === 0){
+        return (
+            <div className="row p-3 justify-content-center">
+                <p>No tenemos productos para mostrarte en esta categoría :(</p>
+            </div>
+        )
+    } else{
+        return (
+            <div className="row p-3 justify-content-center">
+                {products.map((prod) => {
+                    return <ItemProduct product={prod} key={prod.id}/>
+                })}
+            </div>
+        )
+    }
 }
 
 export default ItemList
